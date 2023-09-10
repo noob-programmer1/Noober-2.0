6043e96d71ff5a72e9d75bbe2922e0cd10e8c3c6
Abhishek Agarwal <abhiagarwal16052000@gmail.com>
2023-09-10 21:24:46 +0530

    changed to material 3 Appbar

--- noober/src/commonMain/kotlin/com/abhi165/noober/ui/components/MainAppBar.kt
+++ noober/src/commonMain/kotlin/com/abhi165/noober/ui/components/MainAppBar.kt
@@ -6,17 +6,18 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.TextField
-import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.sharp.ArrowBack
 import androidx.compose.material.icons.sharp.Settings
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -63,6 +64,7 @@ internal fun NoobAppBar(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DefaultAppBar(
     canGoBack: Boolean,
